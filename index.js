1f217b6d2364b02bb56abcd5a13e773480742ad9
Brendan Andrade <brendan@brendanandrade.com>
2023-12-01 08:40:18 -0800

    Day 1 Part 2

--- index.js
+++ index.js
@@ -29,11 +29,45 @@ var answer = data.map((x) => {
         }
     }
     if(!firstDigit || !lastDigit){
-        console.log("Fail: " + x);
         return 0;
     }
     return parseInt(firstDigit.concat(lastDigit));
 }).reduce((sum, x) => sum + x);
 
-console.log(answer);
+console.log("Part 1: " + answer);
+
+var lib = {one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9', zero: '0'};
+
+var part2 = data.map((x) => {
+    if(x.length < 1){
+        return 0;
+    }
+    let firstDigit = null;
+    let lastDigit = null;
+    for ( var i = 0; i < x.length; i++){
+        let detectedDigit = null;
+        const char = x[i];
+        if(char >= '0' && char <= '9'){
+            detectedDigit = char;
+        } else {
+            for(const [key, value] of Object.entries(lib)){
+                if(x.slice(i, i+key.length) === key){
+                    detectedDigit = value;
+                    break;
+                }
+            }
+        }
+        if(detectedDigit){
+            if(!firstDigit){
+                firstDigit = detectedDigit;
+            }
+            lastDigit = detectedDigit;
+        }
+    }
+    return parseInt(firstDigit.concat(lastDigit));
+
+}).reduce((sum, x) => sum + x);
+
+console.log("Part 2: " + part2);
+
 return 0;
