03862c56a4aae1a321f2743763fe0276b2c1f86a
Brendan Andrade <brendan@brendanandrade.com>
2023-12-12 21:00:08 -0800

    Parallelized Day 12 but still too slow

--- day12.js
+++ day12.js
@@ -1,3 +1,5 @@
+var Parallel = require('paralleljs');
+
 if (process.argv.length < 3) {
     console.log('Usage: node ' + process.argv[1] + ' FILENAME');
     process.exit(1);
@@ -20,7 +22,12 @@ try{
 
 console.time("part1");
 
-var part1 = data.reduce((acc, row)=>{
+var p = new Parallel(data);
+function add(d) {return d[0] + d[1]};
+function log() { console.timeEnd("part1"); console.log("Part 1: " + arguments['0']); }
+
+p.map(function(row){
+    var acc = 0;
     var unknowns = row[0].filter(x => x==='?').length
     var line = row[0];
     for(var i = 0; i < 2**unknowns; i++){
@@ -39,6 +46,41 @@ var part1 = data.reduce((acc, row)=>{
     };
     return acc;
     
-},0);
-console.timeEnd("part1")
-console.log("Part 1: " + part1);
+}).reduce(add).then(log);
+
+data = data.map(row=>{
+    var temp1 = row[0];
+    var temp2 = row[1];
+    for(var i = 0; i < 4 ; i++){
+        row[0] = row[0].concat(['?'],temp1);
+        row[1] = row[1].concat(temp2);
+    }
+    return(row)
+})
+
+var p2 = new Parallel(data);
+console.time("part2");
+function log2() { console.timeEnd("part2"); console.log("Part 2: " + arguments['0']); }
+
+
+p2.map(function(row){
+    var acc = 0;
+    var unknowns = row[0].filter(x => x==='?').length
+    var line = row[0];
+    for(var i = 0; i < 2**unknowns; i++){
+        var count = 0;
+        var testLine = line.map((element,) => {
+            if(element === '?'){
+                element = (Math.floor(i / 2**count)) % 2 === 0 ? '.' : '#';
+                count++;
+            }
+            return element
+        })
+        var pattern = testLine.join("").split(/\.+/).filter(x=>x.length>0).map(x=>x.length);
+        if(pattern.length === row[1].length && pattern.every((x,i)=>x === row[1][i])){
+            acc++
+        }
+    };
+    return acc;
+    
+}).reduce(add).then(log2);
